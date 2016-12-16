d8c8198978a0997dd542c6d9464deed28f96e7ca
Peter <peter.lucas@atcl.gg>
2016-12-16 14:30:51 +0000

    Initial commit.

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # ssValidate
+
+Yeah, well, this REALLY is super simple.  I didn't want anything more than "required", but it did have to highlight a select2 autocomplete if it was used.
---
+++ src/ssValidate.js
@@ -0,0 +1,17 @@
+﻿function ssValidate(frm) {
+  $('#errorMSG').hide();
+  $(frm + ' *').removeClass('ss-error');
+  //required fields
+  $(frm + ' [ssValidate~="required"').each(function (i) {
+    if ($(this).val() === "") {
+      $(this).addClass("ss-error");
+      //this caters for select2
+      if ($(this).is("select")) {
+        $(this).next().find("*").addClass("ss-error");
+      }
+    }
+  });
+  //determine if we return true/false
+  if ($(frm + ' .ss-error').length === 0) { return true; }
+  else { $('#errorMSG').show(); return false; }
+}
